468850d00eb1fcd3bdb883b28f8384a852a3c96c
thongnhatgraphic@gmail.com <thongnhatgraphic@gmail.com>
2021-06-17 21:06:46 +0700

    update

--- project1/src/Containers/MainContent/index.js
+++ project1/src/Containers/MainContent/index.js
@@ -2,15 +2,35 @@ import React from 'react';
 import "./index.css"
 // import PropTypes from 'prop-types';
 
-// MainContent.propTypes = {
+// MainContent.propTypes = {sss
 
 // };
 
 function MainContent(props) {
     return (
         <>
-            <div>
-                body
+            <div className="maincontent">
+                <div className="grid--row">
+                    <div className="col pc-3 md-4 mb-6">
+                        <div className="maincontent__img">222
+                            {/* <img src="" alt="books imgs"/> */}
+                        </div>
+                        <p className="maicontent__name">Tiêu đề sách</p>
+                        <div className="maicontent__rate">
+                            <small>5sao</small>
+                            <small>like</small>
+                        </div>
+                    </div>
+                    <div className="col pc-3 md-4 mb-6">
+                        22222
+                    </div>
+                    <div className="col pc-3 md-4 mb-6">
+                        22222
+                    </div>
+                    <div className="col pc-3 md-4 mb-6">
+                        22222
+                    </div>
+                </div>
             </div>
         </>
     );
