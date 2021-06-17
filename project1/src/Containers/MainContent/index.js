f391c9ff7a161a948b2c57e1a66b584a327c2951
thongnhatgraphic <thongnhatgraphic@gmail.com>
2021-06-17 17:51:01 +0700

    ddd

---
+++ project1/src/Containers/MainContent/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./index.css"
+// import PropTypes from 'prop-types';
+
+// MainContent.propTypes = {
+
+// };
+
+function MainContent(props) {
+    return (
+        <>
+            <div>
+                body
+            </div>
+        </>
+    );
+}
+
+export default MainContent;
